1da70419b2bf589fcb9af2b5772f388c82c8ff86
Subha Sadhu <subhasadhu5@gmail.com>
2023-04-28 02:05:02 +0530

    Adding stateful widgets, rendering content conditionally and lifting the state up

---
+++ lib/quiz.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'package:adv_project/start_screen.dart';
+import 'package:adv_project/questions_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    super.initState();
+    activeScreen = StartScreen(switchScreen);
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 165, 82, 255),
+                Color.fromARGB(255, 71, 0, 185),
+              ],
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
