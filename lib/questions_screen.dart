2ae01589c0e8e41ce83edc31267604afb3b4eb6a
Subha Sadhu <subhasadhu5@gmail.com>
2023-04-28 17:19:52 +0530

    Configuring the column and creating a reusable, custom styled button

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,18 +1,47 @@
 import 'package:flutter/material.dart';
 
+import 'package:adv_project/answer_buttons.dart';
+
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
 
   @override
   State<QuestionsScreen> createState() {
-    return _QuestionsScreen(); 
+    return _QuestionsScreen();
   }
 }
 
 class _QuestionsScreen extends State<QuestionsScreen> {
-
   @override
   Widget build(context) {
-    return const Text('Question Screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The question....',
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(height: 30),
+          AnswerButton(
+            answerText: 'Answer 1',
+            onTap: () => {},
+          ),
+          AnswerButton(
+            answerText: 'Answer 2',
+            onTap: () => {},
+          ),
+          AnswerButton(
+            answerText: 'Answer 3',
+            onTap: () => {},
+          ),
+          AnswerButton(
+            answerText: 'Answer 4',
+            onTap: () => {},
+          ),
+        ],
+      ),
+    );
   }
 }
