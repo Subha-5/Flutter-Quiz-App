4af71a986a4ec1fbf597a0e2d5558e17d386cb77
Subha Sadhu <subhasadhu5@gmail.com>
2023-05-01 02:31:32 +0530

    filtering and analyzing lists

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -27,6 +27,12 @@ class ResultsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummary();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -35,11 +41,13 @@ class ResultsScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text('You answered X out of Y questions correctly!'),
+            Text(
+              'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!',
+            ),
             const SizedBox(
               height: 30,
             ),
-            QuestionsSummary(getSummary()),
+            QuestionsSummary(summaryData),
             const SizedBox(
               height: 30,
             ),
