c675c0b1c54bd6d6fa2b70a13d24417437678396
Subha Sadhu <subhasadhu5@gmail.com>
2023-05-01 02:06:21 +0530

    Maps, for loop and type-casting to access data and combining columns and rows to generate result screen

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:adv_project/data/questions.dart';
+import 'package:adv_project/questions_summary.dart';
+
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({
     super.key,
@@ -7,6 +10,20 @@ class ResultsScreen extends StatelessWidget {
   });
 
   final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummary() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].options[0],
+        'user_answer': chosenAnswers[i]
+      });
+    }
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +39,7 @@ class ResultsScreen extends StatelessWidget {
             const SizedBox(
               height: 30,
             ),
-            const Text('List of answers and questions ....'),
+            QuestionsSummary(getSummary()),
             const SizedBox(
               height: 30,
             ),
